3f88772a5f054d1983acfd72f0dc37d778bad30d
Esther Yekini <esther.yekini@force24.co.uk>
2025-09-17 11:59:59 +0100

    testing push to main

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
+// Testing
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
