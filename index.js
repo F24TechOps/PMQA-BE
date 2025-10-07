ed3d385a578c4f6b445a7e3d8222d3d401cdb747
Joe Hill <joe.alex.hill@gmail.com>
2025-10-07 13:45:47 +0100

    Created post endpoint for upload

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
+app.post('/upload', (req, res) => {
+  res.json({ uploadId: 'Jji2XpCSvHT0jyyOHtLc' })
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
