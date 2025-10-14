6c1fad8f6428809e9bab97dd043064e4dcd0ef75
Esther Yekini <esther.yekini@force24.co.uk>
2025-10-14 15:08:48 +0100

    get run endpoint

--- index.js
+++ index.js
@@ -8,17 +8,22 @@ app.get("/", (req, res) => {
   res.json({ status: "ok", APIKey: process.env.CYCLR_API_KEY });
 });
 
-app.get("/api/qa/runs/:id/queue", () => {
+app.get("/api/qa/runs/:id/queue", (req, res) => {
   //Using redis/Bull to queue tasks
   res.json({ queued: true });
 });
 
-app.post('/upload', (req, res) => {
-  res.json({ uploadId: 'Jji2XpCSvHT0jyyOHtLc' })
-})
-app.post('/api/qa/runs', (req, res) => {
-const { uploadId, resultJSON, transactionID } = req.body;
-res.send(`runID: ${runID}`);
+app.get("/api/qa/runs", (req, res) => {
+  res.json({ data: [] });
+});
+
+app.post("/api/upload", (req, res) => {
+  res.json({ uploadId: "Jji2XpCSvHT0jyyOHtLc" });
+});
+
+app.post("/api/qa/run", (req, res) => {
+  const { uploadId, resultJSON, transactionID } = req.body;
+  res.send(`runID: ${runID}`);
 });
 
 const PORT = process.env.PORT || 3000;
