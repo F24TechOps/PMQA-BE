3b79ec90e1b7cb1e206769b40c6e6c0614b509cb
Joe Hill <joe.alex.hill@gmail.com>
2025-10-29 15:50:02 +0000

    checkMapping function completed to go over investigations and sort them into not mapped or null. Further work will be needed to drill down into why it's null in the future. Small fix in checkExpectedFields

--- clients/processing/fieldCheck/checkMapping.js
+++ clients/processing/fieldCheck/checkMapping.js
@@ -1,10 +1,18 @@
 //for the fields that were expected but not in json, check if they are mapped
 
 export default function mappingCheck(
-  expectedFields,
   actualFields,
   comparisonResults
 ) {
   const { resultsObj, furtherInvestigations } = comparisonResults;
+  const itemFields = actualFields.items[0]?.fields || {};
+  for (const field of furtherInvestigations.fields) {
+    if (!itemFields.hasOwnProperty(field)) {
+      resultsObj.fields[field] = { status: "Missing", reason: "Not mapped in Cyclr" };
+      resultsObj.summary.missing++
+    } else {
+      resultsObj.fields[field] = { status: "Missing", reason: "Null" };
+    }
+  }
   return { resultsObj, furtherInvestigations };
 }
