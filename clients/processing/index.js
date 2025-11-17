acabe48a3ef619d607755a6315aed1684580f9a8
Esther Yekini <esther.yekini@force24.co.uk>
2025-11-17 14:53:09 +0000

    step incidents pt 1

--- clients/processing/index.js
+++ clients/processing/index.js
@@ -9,11 +9,84 @@ export default async function runProcessor(
   actualFields,
   transactionContext
 ) {
+  // Compares the expected fields to the actual fields.
   const comparisonResults = compareFields(expectedFields, actualFields);
 
-  const mappingResults = await mappingCheck(comparisonResults, transactionContext);
+  /** Example comparisonResults
+   * 
+   * Results Object ==> {
+  fields: {
+    firstname: { status: 'Null', value: null },
+    lastname: { status: 'Correct', value: 'Rhoades' },
+    jobtitle: { status: 'Correct', value: 'Credit Analyst' },
+    mobilephone: { status: 'Correct', value: '+44 732 431 414' },
+    address1line1: {
+      status: 'Correct',
+      value: '90 Crown St, Camberwell, London SE5, UK'
+    },
+    address1line2: { status: 'Correct', value: 'London SE5' },
+    address1postcode: { status: 'Correct', value: 'SE5' },
+    address1city: { status: 'Correct', value: 'Greater London' },
+    address1county: { status: 'Correct', value: 'England' },
+    address1country: { status: 'Correct', value: 'GB' },
+    xsc9fqry4h: { status: 'Correct', value: 'Barclays Investment Bank' },
+    emailaddress: { status: 'Correct', value: undefined }
+  },
+  summary: { correct: 11, null: 1, missing: 0, warning: 0, error: 0, extra: 0 }
+} Further Investigations Array ==> { fields: [ 'firstname', 'companyname', 'uqjrgk2a6w' ] }
+   * 
+   */
 
-  //checkIncidents - puts fields that were errored in results obj
+  // gets the create and update step (that actually makes the change in the platform and is usually the last step) and checks if the fields that need to be investigated have been mapped. If they have not been mapped then that ends the investigation for that field it gets put in the results. If the field is mapped and still has not come through, it stays in the furtherinvestigations array and is moved on to the next step.
+  const mappingResults = await mappingCheck(
+    comparisonResults,
+    transactionContext
+  );
+
+  /** Example mappingResults
+    * 
+    * Results Object ==> {
+  fields: {
+    firstname: { status: 'Null', value: null },
+    lastname: { status: 'Correct', value: 'Rhoades' },
+    jobtitle: { status: 'Correct', value: 'Credit Analyst' },
+    mobilephone: { status: 'Correct', value: '+44 732 431 414' },
+    address1line1: {
+      status: 'Correct',
+      value: '90 Crown St, Camberwell, London SE5, UK'
+    },
+    address1line2: { status: 'Correct', value: 'London SE5' },
+    address1postcode: { status: 'Correct', value: 'SE5' },
+    address1city: { status: 'Correct', value: 'Greater London' },
+    address1county: { status: 'Correct', value: 'England' },
+    address1country: { status: 'Correct', value: 'GB' },
+    xsc9fqry4h: { status: 'Correct', value: 'Barclays Investment Bank' },
+    emailaddress: { status: 'Correct', value: undefined },
+    uqjrgk2a6w: { status: 'Missing', reason: 'Not Mapped In Cyclr' }
+  },
+  summary: { correct: 11, null: 1, missing: 1, warning: 0, error: 0, extra: 0 }
+} Further Investigations Array ==> {
+  fields: [
+    {
+      id: 'firstname',
+      name: 'First Name',
+      mappingType: 'PreviousStep',
+      isMapped: true,
+      linkedSteps: [Array]
+    },
+    {
+      id: 'companyname',
+      name: 'Company Name',
+      mappingType: 'PreviousStep',
+      isMapped: true,
+      linkedSteps: [Array]
+    }
+  ]
+}
+    * 
+    */
+
+  // checkIncidents - puts fields that were errored in results obj
   const stepResults = stepCheck(
     expectedFields,
     actualFields,
@@ -24,8 +97,8 @@ export default async function runProcessor(
   //send result of this run to db run record
 
   //return results obj
- // console.log(`RESULTS: `, stepResults);
-  return stepResults;
+  // console.log(`RESULTS: `, stepResults);
+  return comparisonResults;
 }
 
 const exampleActualFields = {
@@ -70,7 +143,7 @@ const exampleExpectedFields = [
 const exampleTransactionContext = {
   accountId: "7fcb9aae-c368-46cc-9fd2-4cba6184c90d",
   cycleId: "cb6ff75b-e87c-4602-b63e-d48b3f54ee5a",
-  transactionId: "2d3cd3b3-7957-490f-97ea-17b19f6b7bc3",
+  transactionId: "3440d093-02e5-4a80-a369-aee8eb5b2409",
 };
 
 runProcessor(
